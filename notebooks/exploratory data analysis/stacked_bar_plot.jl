ffeb19073f848d6b62937fe7b42563426faea3ad
bluewhalepig <aeka.tomita@gmail.com>
2025-03-05 14:49:20 -0500

    stack bar plot with 50% abundance filter and other category

--- notebooks/exploratory data analysis/stacked_bar_plot.jl
+++ notebooks/exploratory data analysis/stacked_bar_plot.jl
@@ -1,7 +1,7 @@
 #############################################
 
 # Script for a stacked bar plot of genus abundance data 
-# want t o do this bfre filtering genera 
+# want to do this before filtering genera 
 
 #############################################
 
@@ -70,15 +70,16 @@ mean_abundance = combine(groupby(genus_dataset, :sex)) do df
     DataFrame([mean(df[:, col]) for col in genera]', genera)  # compute the means 
 end
 
-    # x value = sex 
-    # y value = relative abundances colored by genera
+
 #########################
 # Create stacked bar plot 
+    # x = stratified by sex 
+    # y = relative abundances colored by genera 
 
-    # Define empty lists for x-axis, y-axis, and stacking coordinates 
-    xs = Float64[] #sex indexes for each mean abundance 
-    ys = Float64[] #list of mean abundance 
-    stk = Int64[] #genera indexes for each mean abundance 
+# Define empty lists for x-axis, y-axis, and stacking coordinates 
+xs = Float64[] #sex indexes for each mean abundance 
+ys = Float64[] #list of mean abundance 
+stk = Int64[] #genera indexes for each mean abundance 
 
 # Manually generate coordinate inputs for bar plot 
 for i in 1:nrow(mean_abundance)
@@ -101,9 +102,9 @@ ax = Axis(fig[1,1],
 colors = get(ColorSchemes.seaborn_colorblind6, range(0, 1, length=length(genera)))# generate sum cool colors 
 
 # Assign inputs for barplot 
-sex = xs 
-abundance = ys
-genera_stk = stk
+sex = xs # 1... 2...
+abundance = ys # mean abundances 
+genera_stk = stk # 1-60 genera for sex1... 1-60 genera for sex2...
 
 # Create stacked bar plot
 barplot!(
@@ -121,6 +122,97 @@ Legend(fig[1,2], elements, labels, title = "Bacterial Genera", nbanks=3) #nbanks
 
 # Save the figure
 save("data_ext/bacterial_abundance_by_sex.png", fig)
+
+
+
+
+#########################
+# Filter top 50% mean abundnace genera 
+
+# Check first 6 rows of dataset 
+first(genus_dataset, 6)
+
+# Compute mean abundance for each sex category (group by sex)
+mean_abundance = combine(groupby(genus_dataset, :sex)) do df
+    DataFrame([mean(df[:, col]) for col in genera]', genera)  # compute the means 
+end
+
+# Filter out sex column since first row is sex1, second row is sex2 mean abundances 
+mean_abundance = select(mean_abundance, Not(:sex))
+percent = 0.5*size(mean_abundance)[1] # get 50% of abundance 
+counts = [sum(col) for col in eachcol(mean_abundance)] # sum the mean abundances of genera in total 
+bool_filter = counts .> percent
+mean_abundance_filter50 = mean_abundance[:, bool_filter]
+
+# Couple all other mean abundances in OTHER category 
+other_mean_abundance = mean_abundance[:, Not(bool_filter)]
+other = []
+for row in 1:nrow(other_mean_abundance)
+    push!(other, sum(other_mean_abundance[row, :]))
+end 
+
+# Add other column to filtered mean abundance data 
+mean_abundance_filter50.Other = other
+
+mean_abundance = mean_abundance_filter50
+genera = names(mean_abundance)
+#########################
+# Create stacked bar plot 
+    # x = stratified by sex 
+    # y = relative abundances colored by genera 
+    # only show top abundance genera, categorize low abundance in OTHER category 
+
+# Define empty lists for x-axis, y-axis, and stacking coordinates 
+xs = Float64[] #sex indexes for each mean abundance 
+ys = Float64[] #list of mean abundance 
+stk = Int64[] #genera indexes for each mean abundance 
+
+# Manually generate coordinate inputs for bar plot 
+for i in 1:nrow(mean_abundance)
+    for j in 1:ncol(mean_abundance[:,2:end])
+        push!(xs, i) 
+        push!(stk, j)
+        push!(ys, mean_abundance[i,j+1])
+    end
+end 
+
+
+
+# Define colors for each bacterial genera 
+#colors = Makie.wong_colors()[1:length(genera)]  # generate default 'wong' colors 
+colors = get(ColorSchemes.seaborn_colorblind6, range(0, 1, length=length(genera)))# generate sum cool colors 
+
+# Assign inputs for barplot 
+sex = xs # 1... 2...
+abundance = ys # mean abundances 
+genera_stk = stk # 1-60 genera for sex1... 1-60 genera for sex2...
+
+# Create figure and axis
+fig = Figure(size=(1200,800)) #size = choose height and width of fig 
+ax = Axis(fig[1,1], 
+          xticks = (1:2, ["Male", "Female"]),  # Set x-axis labels for sex
+          title = "Bacterial Genus Abundance(50% filter) by Sex",
+          ylabel = "Relative Abundance")
+
+# Create stacked bar plot
+barplot!(
+    ax,
+    sex,
+    abundance,
+    stack = genera_stk,  # Stack the y order for same x coordinate
+    color = [colors[i] for i in genera_stk] # Assign colors for each genera 
+)  
+
+# Create legend
+labels = genera
+elements = [PolyElement(polycolor = colors[i]) for i in 1:length(genera)]
+Legend(fig[1,2], elements, labels, title = "Bacterial Genera") #nbanks = divide legend up into 3 columns 
+
+
+
+# Save the figure
+save("data_ext/filtered_abundance_by_sex.png", fig)
+
 
 
 
