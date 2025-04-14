4805d980899cb476dc732f0bc80ff82222ecc927
bluewhalepig <aeka.tomita@gmail.com>
2025-04-14 14:16:08 -0400

    Load in KHULA sample csv data and create genera abundance dataframe

--- notebooks/Khula-notebook/age_stacked_barplot.jl
+++ notebooks/Khula-notebook/age_stacked_barplot.jl
@@ -5,3 +5,50 @@ using CairoMakie
 using MultivariateStats
 using ColorSchemes
 using Statistics
+
+#Grab the processed KHULA sample csv data 
+dataset = CSV.read("data/HIVmetadata_and_abundance_ZYM.csv", DataFrame)
+
+
+abundance_data = select(dataset, Not(:subject_id,:uid, :zymo_code, :timepoint_id, :mom_arv_selfreport, :baby_arv_selfreport, :medhx_mom___1_selfreport, :mother_hiv_status_phdc))
+
+genus_mapping = Dict() 
+for col in names(abundance_data)
+    if occursin("_", col)
+        genus = split(col, "_")[1]  # extract genus (first part)
+        genus_mapping[col] = genus  # makes it so that species -> genus
+    end
+end 
+
+
+
+# New dataframe for sex, genus abundance dataset (later add genus abundance)
+genus_dataset = dataset  # want to keep subject and sex col
+
+# Get unique genera names 
+genera = unique(values(genus_mapping))
+
+# Sum up species columns based on same genus 
+
+for genus in genera
+    # Find all species columns that belong to this genus
+    species_columns = [species for species in keys(genus_mapping) if genus_mapping[species] == genus]
+    #print(species_columns)
+
+    # Extract select species columns 
+    species_extracted = dataset[!, species_columns]
+
+    # Transform species data frame into matrix
+    species_matrix = Matrix(species_extracted) 
+    
+    # Sum across species columns 
+    genus_sums = sum(species_matrix, dims=2) 
+    #print(genus_sums) #all the genus abundances 
+
+    # Make into 1D vector to insert data back into a column
+    genus_sums_vector = genus_sums[:, 1]  
+    #print(genus_sums_vector) 
+
+    # Store the genus abundances in the genus dataset
+    genus_dataset[!, genus] = genus_sums_vector 
+end 
