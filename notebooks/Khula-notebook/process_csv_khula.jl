6b4313ff50219f16b17d410e8b554d9797bf267b
bluewhalepig <aeka.tomita@gmail.com>
2025-04-14 14:24:37 -0400

    commented out data summary statistics for KHULA

--- notebooks/Khula-notebook/process_csv_khula.jl
+++ notebooks/Khula-notebook/process_csv_khula.jl
@@ -24,8 +24,11 @@ using CSV
 using DataFrames 
 using CairoMakie 
 
-# Import CSV 
-seqdata = CSV.read("data_ext/2025-03-12-SequencingMetadata.csv", DataFrame)
+#####
+# STEP 1: organize the relationship table between subject, zymocode, timepoint and sample ID.
+#####
+
+# Import Sequencing metadata and datarequest table from KHULA 
 
 datarequest = CSV.read("data_ext/2025-03-07-KhulaDataRequest - Sheet1.csv", DataFrame) # ask about password protection
 
@@ -35,11 +38,28 @@ zymo_df = select(datarequest, :zymo_code_3m, :zymo_code_6m,:zymo_code_18m, :zymo
 # select id and biospecimen columns
 seqprep_df = select(seqdata, :uid, :biospecimen, :exclude)
 
-# Filter out exclude == "checked" 
-ismissing_exclude = map(e-> ismissing(e), seqprep_df.exclude)
+# #Data summary 
+    # length(unique(datarequest.subject_id)) #394
+    # total_number_of_infants =nrow(datarequest) #394 infants 
 
-seqprep_df = seqprep_df[ismissing_exclude, :]
+    # # number of mothers with HIV medhx_mom 
+    # unique(datarequest.medhx_mom___1_selfreport)
+    # number_of_infants_with_maternalHIV = count(status -> status == 1, skipmissing(datarequest.unique(datarequest.medhx_mom___1_selfreport)))
+    # percent = number_of_infants_with_maternalHIV / total_number_of_infants #.358
+    #     #33.7% infants with HIV+ mothers (selfreport)
 
+    # #number of mothers with HIV-1 
+    # number_of_HIV1 = count(status -> status == 1, skipmissing(datarequest.hiv_mom_diagnosed_selfreport))
+    #     #92 which means 65.2% of the HIV+ mothers have strain 1 
+    #     number_of_HIV1 / number_of_infants_with_maternalHIV
+    # number_of_HIV2 = count(status -> status == 2, skipmissing(datarequest.hiv_mom_diagnosed_selfreport))
+    #     #16 which means 11.3% 
+    #     number_of_HIV2 / number_of_infants_with_maternalHIV
+    # number_of_HIV3 =count(status -> status == 3, skipmissing(datarequest.hiv_mom_diagnosed_selfreport))
+    #     #3 which means 
+    #     number_of_HIV3 / number_of_infants_with_maternalHIV
+    # unique(datarequest.hiv_mom_diagnosed_selfreport)
+    # (3+16+92)/394
 
 # Merged zymo dataframe
 zyme_merged = stack(zymo_df, Not([]), variable_name = "timepoint", value_name = "zymo_code")
