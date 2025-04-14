ab8fed079043f5494bfe9051b0eecfb4521b36f4
bluewhalepig <aeka.tomita@gmail.com>
2025-04-14 14:14:57 -0400

    loading packages for age stacked barplot

---
+++ notebooks/Khula-notebook/age_stacked_barplot.jl
@@ -0,0 +1,7 @@
+using Leap #must be in Project mode start terminal by:  julia --project=.
+using CSV
+using DataFrames
+using CairoMakie 
+using MultivariateStats
+using ColorSchemes
+using Statistics
